cc8e243faa7c9b528ab7d3e13b27e0944aa8f6bb
Luciano Filardi <filardi.lucianoc@gmail.com>
2025-10-27 21:49:25 -0400

    Search UI implementation

--- src/pages/Transations/index.tsx
+++ src/pages/Transations/index.tsx
@@ -1,12 +1,46 @@
 import { Header } from "../../componentes/Header";
 import { Summary } from "../../componentes/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
-export function Transations( ) {
-    return(
+export function Transations() {
+    return (
 
         <div>
-            <Header/>
-            <Summary/>
+            <Header />
+            <Summary />
+
+            <TransactionsContainer>
+
+                <SearchForm/>
+                
+                <TransactionsTable>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td width="60%">Desenvolvimento de site</td>
+                                <td width={"20%"}>
+                                    <PriceHighlight variant="income">
+                                        R$ 12.000,00
+                                    </PriceHighlight>
+                                </td>
+                                <td>Venda</td>
+                                <td>13/04/2022</td>
+                            </tr>
+                            <tr>
+                                <td width="60%">Alimentação</td>
+                                <td width={"20%"}>
+                                    <PriceHighlight variant="outcome">
+                                        - R$ 60,00
+                                    </PriceHighlight>
+                                </td>
+                                <td>Venda</td>
+                                <td>13/04/2022</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
