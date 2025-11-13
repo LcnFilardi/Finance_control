c4586a83162b6e7140b3adf89909ae5e9ef9a228
Luciano Filardi <filardi.lucianoc@gmail.com>
2025-11-12 23:18:36 -0400

    feat/ Using data on forms from useContext with axios

--- src/pages/Transations/index.tsx
+++ src/pages/Transations/index.tsx
@@ -1,47 +1,63 @@
-import { useContext, useEffect, useState } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { SearchForm } from "../../components/SearchForm";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContextSelector } from "use-context-selector";
 
 export function Transations() {
+  const transactions  = useContextSelector(TransactionsContext, (context) =>  {
+    return context.transactions
+  });
 
-    const {transactions} = useContext(TransactionsContext)    
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-    return (
+      <TransactionsContainer>
+        <SearchForm />
 
-        <div>
-            <Header />
-            <Summary />
+        <TransactionsTable>
+          <table>
+            {/* 🧭 Cabeçalho da tabela */}
+            <thead>
+              <tr>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
 
-            <TransactionsContainer>
-
-                <SearchForm />
-
-                <TransactionsTable>
-                    <table>
-                        <tbody>
-                            {transactions.map(transaction => {
-                                return (
-                                    <tr key={transaction.id}>
-                                        <td width="60%">{transaction.description}</td>
-                                        <td width={"20%"}>
-                                            <PriceHighlight variant={transaction.type}>
-                                                {transaction.type === 'exit' && '- '}
-                                                {priceFormatter.format(transaction.value)}
-                                            </PriceHighlight>
-                                        </td>
-                                        <td>{transaction.category}</td>
-                                        <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+            <tbody>
+              {transactions.map((transaction) => {
+                return (
+                  <tr key={transaction.id}>
+                    <td width="60%">{transaction.description}</td>
+                    <td width={"20%"}>
+                      <PriceHighlight variant={transaction.type}>
+                        {transaction.type === "exit" && "- "}
+                        {priceFormatter.format(transaction.value)}
+                      </PriceHighlight>
+                    </td>
+                    <td>{transaction.category}</td>
+                    <td>
+                      {dateFormatter.format(new Date(transaction.createdAt))}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
